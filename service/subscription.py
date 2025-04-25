f5967410195323e4bd1e9de9256728e509b6061d
yohaneswijaya12 <johannes.setiawan@ui.ac.id>
2025-04-25 17:53:49 +0700

    Singleton in send email

--- service/subscription.py
+++ service/subscription.py
@@ -3,7 +3,10 @@ from dotenv import load_dotenv
 import os
 from service.email import *
 from datetime import datetime, timezone
+import asyncio
+
 load_dotenv()
+send_email_lock = asyncio.Lock()
 
 conf = ConnectionConfig(
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME"),
@@ -16,40 +19,41 @@ conf = ConnectionConfig(
     TEMPLATE_FOLDER = './templates',
 )
 
-async def send_email(topic, values) -> None:  
-    emails_dict = await get_email_subscriptions()
-    curr_time = int(datetime.now(timezone.utc).timestamp()) 
-    email = []
-    email_id=[]
-    for emails in emails_dict:
-        if curr_time - emails['last_sent'] > 60:
-            email.append(emails['email'])
-            email_id.append(emails['_id'])
+async def send_email(topic, values) -> None:
+    async with send_email_lock:
+        emails_dict = await get_email_subscriptions()
+        curr_time = int(datetime.now(timezone.utc).timestamp()) 
+        email = []
+        email_id=[]
+        for emails in emails_dict:
+            if curr_time - emails['last_sent'] > 60:
+                email.append(emails['email'])
+                email_id.append(emails['_id'])
 
-    if len(email) > 0:
+        if len(email) > 0:
+            if topic == "DOS":
+                body = {
+                    "prediction": topic,
+                    "timestamp": values['TIMESTAMP_START'],
+                    "srcIp": values['IP_DST'],
+                    "timestamp_end": values['TIMESTAMP_END']
+                }  
+            elif topic == "PORT_SCAN":
+                body = {
+                    "prediction": topic,
+                    "timestamp": values['TIMESTAMP_START'],
+                    "srcIp": values['IP_SRC'],
+                    "timestamp_end": values['TIMESTAMP_END']
+                }
 
-        if topic == "DOS":
-            body = {
-                "prediction": topic,
-                "timestamp": values['TIMESTAMP_START'],
-                "srcIp":values['IP_DST'],
-                "timestamp_end":values['TIMESTAMP_END']
-            }  
-        elif topic == "PORT_SCAN":
-            body = {
-                "prediction": topic,
-                "timestamp": values['TIMESTAMP_START'],
-                "srcIp":values['IP_SRC'],
-                "timestamp_end":values['TIMESTAMP_END']
-            }
-        message = MessageSchema(
-            subject="Intrusion Detected!",
-            recipients=email,
-            template_body=body,
-            subtype=MessageType.html,
+            message = MessageSchema(
+                subject="Intrusion Detected!",
+                recipients=email,
+                template_body=body,
+                subtype=MessageType.html,
             )
 
-        fm = FastMail(conf)
-        await fm.send_message(message, template_name="email.html")
-        await update_email_subscription(email_id)
+            fm = FastMail(conf)
+            await fm.send_message(message, template_name="email.html")
+            await update_email_subscription(email_id)
     return
